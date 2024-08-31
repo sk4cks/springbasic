9281e954a810c1d931fec4cd4dacc08f2cb367ae
승찬 나 <sk4cks@naver.com>
2024-08-31 17:52:14 +0900

    chore: init

---
+++ src/main/java/toby_project/springbasic/SpringbasicApplication.java
@@ -0,0 +1,13 @@
+package toby_project.springbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toby_project/springbasic/SpringbasicApplicationTests.java
@@ -0,0 +1,13 @@
+package toby_project.springbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
