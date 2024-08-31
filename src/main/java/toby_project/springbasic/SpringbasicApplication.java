1c6a6dcd3125a24c800aac52a02f37475225a0c7
승찬 나 <sk4cks@naver.com>
2024-08-31 20:47:22 +0900

    feat: 서블릿 컨테이너 올리기 연습

--- src/main/java/toby_project/springbasic/SpringbasicApplication.java
+++ src/main/java/toby_project/springbasic/SpringbasicApplication.java
@@ -1,13 +1,42 @@
 package toby_project.springbasic;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class SpringbasicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbasicApplication.class, args);
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					String name = req.getParameter("name");
+
+					resp.setStatus(HttpStatus.OK.value());
+					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+					resp.getWriter().println("Hello "+name);
+				}
+			}).addMapping("/hello");
+		});
+		webServer.start();
+
+//		SpringApplication.run(SpringbasicApplication.class, args);
 	}
 
 }
---
+++ src/main/java/toby_project/springbasic/hello/controller/HelloController.java
@@ -0,0 +1,13 @@
+package toby_project.springbasic.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(String name){
+        return "Hello "+name;
+    }
+}
