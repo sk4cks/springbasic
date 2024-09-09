dd5f332b049db182dd7d33b3ccf7853b86bd14fc
승찬 나 <sk4cks@naver.com>
2024-09-09 22:43:34 +0900

    feat: 자동 구성 어노테이션 적용

--- src/main/java/toby_project/config/MyAutoConfigImportSelector.java
+++ src/main/java/toby_project/config/MyAutoConfigImportSelector.java
@@ -1,14 +1,26 @@
 package toby_project.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[] {
-                "toby_project.config.autoconfig.DispatcherServletConfig",
-                "toby_project.config.autoconfig.TomcatWebServerConfig"
-        };
+        List<String> autoConfigs = new ArrayList<>();
+
+        ImportCandidates.load(MyAutoConfiguration.class,classLoader).forEach(autoConfigs::add);
+
+        return autoConfigs.toArray(new String[0]);
     }
 }
--- src/main/java/toby_project/config/autoconfig/DispatcherServletConfig.java
+++ src/main/java/toby_project/config/autoconfig/DispatcherServletConfig.java
@@ -3,8 +3,9 @@ package toby_project.config.autoconfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
+import toby_project.config.MyAutoConfiguration;
 
-@Configuration
+@MyAutoConfiguration
 public class DispatcherServletConfig {
 
     @Bean
