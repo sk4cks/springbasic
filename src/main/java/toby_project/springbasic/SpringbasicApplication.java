cffb16e8b7e36da3ac0ea4b99d4b4b700ca88987
승찬 나 <sk4cks@naver.com>
2024-10-10 21:29:58 +0900

    feat: 자동 구성 조건 결과 확인 및 살펴보기

--- src/main/java/toby_project/springbasic/SpringbasicApplication.java
+++ src/main/java/toby_project/springbasic/SpringbasicApplication.java
@@ -1,8 +1,11 @@
 package toby_project.springbasic;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionEvaluationReport;
+import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
@@ -16,6 +19,22 @@ public class SpringbasicApplication {
 	@PostConstruct
 	void init() {
 		jdbcTemplate.execute("create table if not exists hello(name varchar(50) primary key, count int)");
+	}
+
+	@Bean
+	ApplicationRunner run(ConditionEvaluationReport report) {
+		return args -> {
+			System.out.println(report.getConditionAndOutcomesBySource().entrySet().stream()
+					.filter(co -> co.getValue().isFullMatch())
+					.map(co -> {
+						System.out.println(co.getKey());
+						co.getValue().forEach(c -> {
+							System.out.println("\t"+c.getOutcome());
+						});
+						System.out.println();
+						return co;
+					}).count());
+		};
 	}
 
 	public static void main(String[] args) {
